4fafad4c67f11ef8a74e71d70e90e4d9ff066d1b
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2018-07-13 23:47:15 -0400

    adding second solution to array chunking

--- chunk/index.js
+++ chunk/index.js
@@ -12,14 +12,28 @@ function chunk(array, size) {
 	//array to hold all the data
 	const chunked = [];
 
-	for(let element of array){
-		var last = chunked[chunked.length -1];
-		if(!last || last.length === size){
-			chunked.push([element]);
-		}else{
-			last.push(element);
-		}
+
+									// SOLUTION 1
+
+	var index = 0;
+
+	while(index < array.length){
+		chunked.push(array.slice(index,index + size));
+		index += size;
 	}
+
+
+
+									// SOLUTION 2
+
+	// for(let element of array){
+	// 	var last = chunked[chunked.length -1];
+	// 	if(!last || last.length === size){
+	// 		chunked.push([element]);
+	// 	}else{
+	// 		last.push(element);
+	// 	}
+	// }
 
 	return chunked;
 }
