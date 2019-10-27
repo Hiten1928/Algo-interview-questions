eaf107e257585681458bad6199a0bad595761274
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2019-10-27 02:21:27 -0400

    Spiral Matrix

--- matrix/index.js
+++ matrix/index.js
@@ -15,6 +15,48 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  var results = []
+
+  for (let i = 0; i < n; i++) {
+    results.push([])
+  }
+
+  let count = 1
+
+  let currColum,
+    startColumn = 0,
+    startRow = 0,
+    endRow = n - 1,
+    endColumn = n - 1
+
+  while (startColumn <= endColumn && startRow <= endRow) {
+    //Top row
+    for (let j = startColumn; j <= endColumn; j++) {
+      results[startRow][j] = count
+      count++
+    }
+    startRow++
+    //Right column
+    for (let k = startRow; k <= endRow; k++) {
+      results[k][endColumn] = count
+      count++
+    }
+    endColumn--
+    //Bottom Row
+    for (let i = endColumn; i >= startColumn; i--) {
+      results[endRow][i] = count
+      count++
+    }
+    endRow--
+    //1st Row back
+    for (let i = endRow; i >= startRow; i--) {
+      results[i][startColumn] = count
+      count++
+    }
+    startColumn++
+  }
+  return results
+}
 
 module.exports = matrix
