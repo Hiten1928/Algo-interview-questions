1bfd8ec69d42f2d02b411d7730e9f1c87b7685f6
Hiten1928 <31774329+Hiten1928@users.noreply.github.com>
2020-03-10 02:05:49 -0400

    Disjoint Intervals

---
+++ DisjointIntervals/disjointIntervals.js
@@ -0,0 +1,31 @@
+var summaryRanges = function() {
+    this.array = [];
+}
+
+summaryRanges.prototype.addNum = function(num) {
+    this.array.push(num);
+}
+
+summaryRanges.prototype.getIntervals = function() {
+    let temp = [], start = -1
+
+    for(let i =0; i<this.array.length; i++) {
+        if(start == -1){
+            start = i;
+            continue;
+        } else {
+            if(!this.array[i]){
+                temp.push(start,i-1)
+                start = -1;
+                continue;
+            }
+        }
+    }
+
+    if(start !== -1){
+        temp.push(start, this.array.length - 1);
+    }
+
+    return temp
+}
+
